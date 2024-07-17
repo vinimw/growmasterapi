0b676345bdd35f66191424c08fd935e041920a27
Vinicius Weber <vinicius.weber@contabilizei.com.br>
2024-07-17 03:07:46 -0300

    feat: update config heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: hypercorn app:app --bind 0.0.0.0:$PORT
--- app.py
+++ app.py
@@ -20,4 +20,6 @@ async def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
