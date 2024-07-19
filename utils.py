438d5d464c4b2bc14816d355e60f308b3c6d052f
Vinicius Weber <vinicius.weber@contabilizei.com.br>
2024-07-19 15:05:53 -0300

    feat: delete feature added

--- utils.py
+++ utils.py
@@ -78,3 +78,27 @@ async def patch_external_data(url, data, headers=None):
     except aiohttp.ClientError as e:
         logging.error(f"Client error: {str(e)}")
         return {'error': str(e)}, 500
+
+def build_request_params_delete(endpoint, id_delete):
+    base_url = config.endpoints[endpoint]
+    url = f"{base_url}/{id_delete}"
+    headers = {"x-client-id": config.clientId}
+    return url, headers
+
+async def delete_external_data(url, headers=None):
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    conn = aiohttp.TCPConnector(ssl=ssl_context)
+    try:
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with session.delete(url, headers=headers) as response:
+                logging.debug(f"Response status: {response.status}")
+                if response.status == 200:
+                    return await response.json(), 200
+                else:
+                    return {'error': config.errorCodes[701]}, response.status
+    except aiohttp.ClientError as e:
+        logging.error(f"Client error: {str(e)}")
+        return {'error': str(e)}, 500
--- views/grow_views.py
+++ views/grow_views.py
@@ -1,5 +1,5 @@
 from quart import Blueprint, request, jsonify
-from utils import fetch_external_data, post_external_data, build_request_params, build_request_params_post, build_request_params_patch, patch_external_data
+from utils import fetch_external_data, post_external_data, build_request_params, build_request_params_post, build_request_params_patch, patch_external_data, build_request_params_delete, delete_external_data
 from auth import token_required
 
 grow_bp = Blueprint('grow_bp', __name__)
@@ -80,6 +80,36 @@ async def edit_plant():
 
     url, headers = build_request_params_patch('grows', id_update)
     external_data, status = await patch_external_data(url, dataSend, headers)
+    
+    if status != 200:
+        return jsonify(external_data), status
+
+    return jsonify(external_data)
+
+@grow_bp.route('/api/grows/delete', methods=['POST'])
+@token_required
+async def edit_plant():
+    auth_header = request.headers.get('Authorization')
+    token = auth_header.split('Bearer ')[1]
+
+    data = await request.get_json()
+    
+    # Lista de campos obrigatórios
+    required_fields = ['id']
+
+    # Verificação de campos ausentes
+    missing_fields = [field for field in required_fields if not data.get(field)]
+
+    if missing_fields:
+        return jsonify({'error': 'Missing required fields', 'fields': missing_fields}), 400
+
+    id = data.get('id')
+
+    if id == '':
+        return jsonify({'error': 'Missing required fields', 'field id must be different': 'empty'}), 400
+
+    url, headers = build_request_params_delete('grows', id)
+    external_data, status = await delete_external_data(url, headers)
     
     if status != 200:
         return jsonify(external_data), status
